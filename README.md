ae1e0b8234e6799a54adc932fe387aa18bfbe03d
NINEZERN <gleb.devyatka@gmail.com>
2024-04-29 20:39:31 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Shopping Product List App
+
+![App Screenshot](images/screenshot.png){:width="400px" height="300px"}
+
+## Overview
+
+This is a simple web application built using JavaScript, HTML, CSS, and Firebase to manage your shopping product list. With this app, you can easily add, remove, and update products on your shopping list, all in real-time thanks to Firebase's powerful database capabilities.
+
+## Features
+
+- **Real-time Updates**: Changes made to the shopping list are instantly reflected across all connected devices.
+
+- **Add and Remove Products**: Easily add new products to your list or remove existing ones with a simple click.
+
+## Getting Started
+
+To get started with this app, follow these steps:
+
+```bash
+# Clone the repository to your local machine
+git clone https://github.com/your-username/shopping-product-list.git
+
+# Navigate to the project directory
+cd shopping-product-list
+```
+
+## Firebase Setup
+
+This app uses Firebase for real-time database functionality and user authentication. Follow these steps to set up Firebase for your project:
+
+1. Create a Firebase project on the Firebase Console.
+2. Enable Firestore database and Firebase Authentication for your project.
+3. Replace the dataBaseURL value to your database link:
+
+```javascript
+// Your web app's Firebase configuration
+const appSettings = {
+  databaseURL: "YOUR DATABASE URL",
+};
+```
+
+## Try It Out
+
+This app is hosted at [URL](https://ninezern-shop.netlify.app/), where you can try it out!
---
+++ src/index.js
@@ -0,0 +1,76 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+// DB Variables
+const appSettings = {
+  databaseURL: "https://realtime-database-3f09f-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDatabase = ref(database, "shoppingList");
+
+// On DB Update
+onValue(shoppingListInDatabase, function(snapshot) {
+  if (!snapshot.exists()) {
+    shoppingListEl.innerHTML = "No items here... yet";
+    shoppingListEl.style.fontSize = "20px";
+    return;
+  }
+  let shoppingListArray = Object.entries(snapshot.val());
+
+  shoppingListEl.innerHTML = "";
+
+  for (let i = 0; i < shoppingListArray.length; i++) {
+    let currentItem = shoppingListArray[i];
+    let currentItemID = currentItem[0];
+    let currentItemValue = currentItem[1];
+    addItemToShoppingList(currentItem);
+  }
+})
+
+// HTML Variables
+const addButtonEl = document.getElementById("add-button");
+const inputFieldEl = document.getElementById("input-field");
+const shoppingListEl = document.getElementById("shopping-list");
+
+
+// On button click
+addButtonEl.addEventListener("click", function() {
+  let inputValue = inputFieldEl.value;
+  if (inputValue === "") {
+    alert("You must write something");
+    return;
+  }
+  console.log(inputValue);
+  addItemToShoppingList(inputValue);
+  addItemToDatabase(inputValue);
+  inputFieldEl.value = "";
+});
+
+// On Item click
+
+
+function addItemToDatabase(item) {
+  push(shoppingListInDatabase, item);
+}
+
+function addItemToShoppingList(item) {
+  let itemID = item[0];
+  let itemValue = item[1];
+  
+
+  let newEl = document.createElement("li");
+
+  
+  newEl.textContent = itemValue;
+
+  newEl.addEventListener("click", function() {
+    let exactLocationOfItemInDatabase = ref(database, `shoppingList/${itemID}`);
+    remove(exactLocationOfItemInDatabase);
+   
+  }
+  )
+  shoppingListEl.append(newEl);
+  
+}
